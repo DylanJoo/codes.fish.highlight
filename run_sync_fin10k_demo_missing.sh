866c407e0349b3f547aa7de5143fd6b039b80547
DylanJoo <dylanjootw@gmail.com>
2022-09-04 06:42:50 +0800

    add type0 and type1 missing

--- run_sync_fin10k_demo_missing.sh
+++ run_sync_fin10k_demo_missing.sh
@@ -1,11 +1,25 @@
 for PATH_COM in fin10k-missing-data/*;do
     COM=${PATH_COM##*/}
-    for PATH_FILE in fin10k-missing-data/${COM}/*.pred;do
-        FILE=${PATH_FILE##*/}
+    # for PATH_FILE in fin10k-missing-data/${COM}/*.pred;do
+    #     FILE=${PATH_FILE##*/}
+    #     ORIG=${PATH_FILE/fin10k-missing-data/fin10k-results}
+    #     MISSING=${PATH_FILE}
+    #     # wc -l $MISSING
+    #     # wc -l $ORIG
+    #     cat $MISSING >> $ORIG
+    # done
+    for PATH_FILE in fin10k-missing-data/${COM}/*.type0;do
         ORIG=${PATH_FILE/fin10k-missing-data/fin10k-results}
         MISSING=${PATH_FILE}
-        # wc -l $MISSING
-        # wc -l $ORIG
         cat $MISSING >> $ORIG
+        # wc -l $ORIG
+        # wc -l $MISSING
+    done
+    for PATH_FILE in fin10k-missing-data/${COM}/*.type1;do
+        ORIG=${PATH_FILE/fin10k-missing-data/fin10k-results}
+        MISSING=${PATH_FILE}
+        cat $MISSING >> $ORIG
+        # wc -l $ORIG
+        # wc -l $MISSING
     done
 done
