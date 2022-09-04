7af489090a4275d05b4969e388261ca921ef0bff
DylanJoo <dylanjootw@gmail.com>
2022-09-04 19:14:35 +0800

    add type0/type1 previous data

--- run_sync_fin10k_demo.sh
+++ run_sync_fin10k_demo.sh
@@ -3,14 +3,14 @@ for PATH_COM in fin10k-data/*;do
     for PATH_FILE in fin10k-data/${COM}/*.type0;do
         FROM=${PATH_FILE}
         RESULT=${PATH_FILE/fin10k-data/fin10k-results}
-        cat $FROM >> $TO
+        cat $FROM >> $RESULT
         # wc -l $FROM
         # wc -l $RESULT
     done
     for PATH_FILE in fin10k-data/${COM}/*.type1;do
         FROM=${PATH_FILE}
         RESULT=${PATH_FILE/fin10k-data/fin10k-results}
-        cat $FROM >> $TO
+        cat $FROM >> $RESULT
         # wc -l $FROM
         # wc -l $RESULT
     done
